d92473af564b302de1e5aa273961de6e796a2123
Shrinidhi Hegde <shrinidhihegde949@gmail.com>
2026-05-06 18:09:21 +0530

    mongodb fix

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "081e5464b59206e9"; }
+/******/ 		__webpack_require__.h = function() { return "a367c46c019a7eb2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
